8e10023353d3a952ace3834da00757cfb2021325
Jordan Rahim <enderbyte09@gmail.com>
2026-01-17 21:50:07 -0800

    Add files via upload

--- speedtest.py
+++ speedtest.py
@@ -1,7 +1,14 @@
-import xml.etree.ElementTree
+import xml.sax
 import json
-FILE = "Vancouver.osm"
-
+import io
+import sys
+import os
+import random
+FILE = sys.argv[1]
+TEMPID = str(random.randint(1111,9999))
+print("Converting")
+os.system(f"osmfilter.exe {FILE} --keep=\"maxspeed\" -o=\"{TEMPID}ready.osm\"")
+FILE = f"{TEMPID}ready.osm"
 def parse_speed(i:str) -> int:
     try:
         return int(i)
@@ -19,41 +26,86 @@ def parse_speed(i:str) -> int:
             except:
                 return -2#Unparseable
 
+def coordinate_to_bytes(coord:float) -> bytes:
+    return int(coord * 10 ** 7).to_bytes(signed=True,length=4)
+
+def bytes_to_coordinate(coord:bytes) -> float:
+    return int.from_bytes(coord,signed=True) / 10 ** 7
+
 class SpeedWay:
     def __init__(self):
         self.nodes:list[list[int]] = []
         self.maxspeed:int = -1
         self.conditional_speed = -1
+        self.advisory_speed = -1
         self.name:str = "Unnamed Way"
 
-print("Loading data")
-tree = xml.etree.ElementTree.parse(FILE)
-
-root = tree.getroot()
-print("Parsing data")
-
-nodes:dict[int,list] = {}
 all_ways:list[SpeedWay] = []
+node_id_index:list[int] = []
 
-for child in root:
-    if child.tag == "node":
-        nodes[child.attrib["id"]] = [child.attrib["lat"],child.attrib["lon"]]
+class OSMHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.current_element = ""
+        self.current_way = SpeedWay()
+        self.is_on_way_mode = False
+        self.current_way_is_hwy = False
+        self.node_file = io.BytesIO()
+        self.nodeincrement = 0
+        self.found_ways = 0
 
-    if child.tag == "way":
+    # Called when an element starts
+    def startElement(self, tag, attrib):
+        global node_id_index
+        global all_ways
+        self.current_element = tag
+        
+        if tag == "node":
+            nodeid = int(attrib["id"])
+            node_id_index.append(nodeid)
+            notelat = float(attrib["lat"])
+            notelon = float(attrib["lon"])
+            self.node_file.write(coordinate_to_bytes(notelat))
+            self.node_file.write(coordinate_to_bytes(notelon))
 
-        tw = SpeedWay()
-        for subitem in child:
-            if subitem.tag == "nd":
-                tw.nodes.append(nodes[subitem.attrib["ref"]])
-            if subitem.tag == "tag":
-                if subitem.attrib["k"] == "name":
-                    tw.name = subitem.attrib["v"]
-                elif subitem.attrib["k"] == "maxspeed":
-                    tw.maxspeed = parse_speed(subitem.attrib["v"])
-                elif subitem.attrib["k"] == "maxspeed:conditional":
-                    tw.conditional_speed = parse_speed(subitem.attrib["v"])
-        if tw.maxspeed != -1:
-            all_ways.append(tw)
+        if tag == "way":
+            self.current_way = SpeedWay()
+            self.is_on_way_mode = True
+            self.current_way_is_hwy = False
+        if tag == "nd":
+            nl = [0,0]
+            seekid = int(attrib["ref"])
+            location = node_id_index.index(seekid)
+            self.node_file.seek(location*8,0)
+            nl[0] = bytes_to_coordinate(self.node_file.read(4))
+            nl[1] = bytes_to_coordinate(self.node_file.read(4))
+            self.current_way.nodes.append(nl)
+        if tag == "tag":
+            if attrib["k"] == "name":
+                self.current_way.name = attrib["v"]
+            elif attrib["k"] == "maxspeed":
+                self.current_way.maxspeed = parse_speed(attrib["v"])
+            elif attrib["k"] == "maxspeed:conditional":
+                self.current_way.conditional_speed = parse_speed(attrib["v"])
+            elif attrib["k"] == "maxspeed:advisory":
+                self.current_way.advisory_speed = parse_speed(attrib["v"])
+            elif attrib["k"] == "highway":
+                self.current_way_is_hwy = True
+            
+
+    # Called when an element ends
+    def endElement(self, tag):
+        global all_ways
+        if self.current_way.maxspeed != -1 and self.is_on_way_mode and tag == "way" and self.current_way_is_hwy:
+            sys.stdout.write(self.current_way.name+" ")
+            sys.stdout.flush()
+            self.found_ways += 1
+            #print(self.found_ways,end="\r")
+            all_ways.append(self.current_way)
+
+parser = xml.sax.make_parser()
+parser.setContentHandler(OSMHandler())
+print("Parsing data")
+parser.parse(FILE)
 
 final_list = []
 
@@ -62,8 +114,9 @@ for way in all_ways:
         "name" : way.name,
         "speed" : way.maxspeed,
         "conditional_speed" : way.conditional_speed,
+        "advisory_speed" : way.advisory_speed,
         "nodes" : way.nodes
     })
 
-with open("speedout.json","w+") as f:
+with open(sys.argv[2],"w+") as f:
     f.write(json.dumps(final_list))
