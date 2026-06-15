ea3038bbf17e7d32ea2bbaf93f272ebcfb856ab4
Jordan Rahim <enderbyte09@gmail.com>
2026-06-15 15:52:51 -0700

    working

--- speedtest-osmium.py
+++ speedtest-osmium.py
@@ -1,14 +1,30 @@
+#type:ignore
 import sys
 import os
-import random
+import threading
 import osmium
 import osmium.osm
-import hashlib
 import osmium.index
 import typing
+import time
+import argparse
 
-FILE = sys.argv[1]
-TEMPID = str(random.randint(1111,9999))
+parser = argparse.ArgumentParser("speedtest-osmium.py")
+parser.add_argument("-a","--append",action="store_true",help="Add to way lists instead of overwriting them",required=False)
+parser.add_argument("-d","--diskcache",action="store_true",help="Cache nodes on the disk instead of in memory")
+parser.add_argument("filename",action="store",type=str)
+args = parser.parse_args(sys.argv[1:])
+
+FILE = args.filename
+ISFINISHED = False
+start_time = time.time()
+ways_found = 0
+file_open_mode = "a+" if args.append else "w+"
+location_storage_implementation = "sparse_file_array" if args.diskcache else "flex_mem"
+
+if not FILE.endswith("osm") and not FILE.endswith("pbf"):
+    print("bad filetype")
+    sys.exit(-1)
 
 os.makedirs("output",exist_ok=True)
 
@@ -44,10 +60,7 @@ class SpeedWay:
         return "&".join([f"{z[0]}*{z[1]}" for z in self.nodes])
     
     def format_for_output(self):
-        return f"{self.name},{self.maxspeed},{self.conditional_speed},{self.advisory_speed},{self._nodelist_tostr()}"
-    
-    def get_hash(self):
-        return hashlib.sha256(self.format_for_output().encode("utf-8")).hexdigest()
+        return f"{self.name},{self.maxspeed},{self.conditional_speed},{self.advisory_speed},{self._nodelist_tostr()}\n"
 
 files:dict[str,typing.TextIO] = {}
 node_id_index:list[int] = []
@@ -56,16 +69,23 @@ def demand_write(way:SpeedWay):
     filepath = way.get_filestring()
 
     if not filepath in files:
-        files[filepath] = open("output/"+filepath,"a+",encoding="utf-8")
+        files[filepath] = open("output/"+filepath,file_open_mode,encoding="utf-8")
 
-    files[filepath].write(way.format_for_output() + "," + way.get_hash() + ",\n")
+    files[filepath].write(way.format_for_output())
 
+def progress_thread():
+    time.sleep(1)
+    while not ISFINISHED:
+        tdelta = time.time() - start_time
+        print(f"{ways_found} in {round(tdelta)}s ({round(ways_found/tdelta)} ways/s)",end="\r")
+        time.sleep(1)
 class WayHandler(osmium.SimpleHandler):
     def way(self,available:osmium.osm.Way):
+        global ways_found
         if not "maxspeed" in available.tags:
             return
         way = SpeedWay()
-        way.nodes = [[p.location.lon,p.location.lat] for p in available.nodes]
+        way.nodes = [[p.location.lat,p.location.lon] for p in available.nodes]
         way.maxspeed = parse_speed(available.tags.get("maxspeed"))
         if "maxspeed:conditional" in available.tags:
             way.conditional_speed = parse_speed(available.tags.get("maxspeed:conditional"))
@@ -73,12 +93,18 @@ class WayHandler(osmium.SimpleHandler):
             way.conditional_speed = parse_speed(available.tags.get("maxspeed:advisory"))
         if "name" in available.tags:
             way.name = available.tags.get("name").replace(",","")
+        ways_found += 1
         demand_write(way)
 
 #fp = osmium.FileProcessor(FILE).with_filter(osmium.filter.EntityFilter(osmium.osm.WAY)).with_filter(osmium.filter.KeyFilter("maxspeed"))
-location_cache = osmium.index.create_map("flex_mem")
+location_cache = osmium.index.create_map(location_storage_implementation)
 reader_wrapper = osmium.NodeLocationsForWays(location_cache)
+print("Loading nodes. Please wait...")
+threading.Thread(target=progress_thread).start()
 osmium.apply(FILE,reader_wrapper,WayHandler())
+
+print("\n\nCompleted")
+ISFINISHED = True
 
 for file in files.values():
     file.close()
